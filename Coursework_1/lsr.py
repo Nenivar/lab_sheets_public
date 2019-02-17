8398c3429ae0cd07282a9b588e79652867213a1b
Nenivar <nenivar@gmail.com>
2019-02-17 16:37:11 +0000

    higher powers?

--- Coursework_1/lsr.py
+++ Coursework_1/lsr.py
@@ -18,6 +18,28 @@ class Segment():
     def __init__(self, xs, ys):
         self.xs, self.ys = xs, ys
     
+    def leastSquaresPoly(self, p):
+        # (X^t * X)^-1 * X^T * y
+
+        X = np.empty((0, 20))
+        for deg in range(0, p + 1):
+            row = list(map(lambda x: x ** deg, self.xs))
+            X = np.insert(X, deg, row, 0)
+        
+        #print(X)
+        #print(self.ys)
+        H = np.linalg.inv(np.matmul(X, X.T))
+        #print(H)
+        #print(H.shape)
+        C = np.matmul(X, self.ys)
+        #print(C)
+        #print(H.shape, C.shape)
+        R = np.matmul(H, C)
+        #print(R)
+
+        coeff = []
+        return R
+    
     def leastSquares(self):
         x, y = copy(self.xs), copy(self.ys)
 
@@ -40,6 +62,14 @@ class Segment():
         #ax.plot(self.xs, self.ys)
         width = np.linspace(self.xs[0], self.xs[len(self.xs) - 1], 50)
         ax.plot(width, b * width + a)
+    
+    def plotPoly(self, ax, coeff):
+        width = np.linspace(self.xs[0], self.xs[len(self.xs) - 1], 50)
+        yVals = 0
+        for i in range(0, len(coeff)):
+            yVals += coeff[i] * (width ** i)
+        #print(yVals)
+        ax.plot(width, yVals)
 
 # split into segments
 segments = []
@@ -51,13 +81,17 @@ ax = plt.axes()
 # for each segment...
 for seg in segments:
     # determine the function type
-    ans = seg.leastSquares()
+    ans = seg.leastSquaresPoly(3)
+    #ansP = seg.leastSquaresPoly(1)
+    #print('A & B', ans, ansP)
+
     # produce the total reconstruction error
-    err = seg.residual(ans[0][0], ans[1][0])
+    err = seg.residual(ans[0], ans[1])
     print(err)
     # plot line if app.
     if plot:
-        seg.plot(ax, ans[0], ans[1])
+        #seg.plot(ax, ans[0], ans[1])
+        seg.plotPoly(ax, ans)
 
 # produce a figure w/ reconstructed line
 if plot:
